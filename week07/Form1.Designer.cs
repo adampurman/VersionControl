807083ce6a5e129cb17721452cdef62aeea86f3b
Ádám <adam.purman@stud.uni-corvinus.hu>
2020-10-23 14:03:19 +0200

    third commit

--- week07/Form1.Designer.cs
+++ week07/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.csvpathtxt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.eredmenytxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.zaroev = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.zaroev)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // csvpathtxt
+            // 
+            this.csvpathtxt.Location = new System.Drawing.Point(263, 33);
+            this.csvpathtxt.Margin = new System.Windows.Forms.Padding(2);
+            this.csvpathtxt.Name = "csvpathtxt";
+            this.csvpathtxt.Size = new System.Drawing.Size(274, 20);
+            this.csvpathtxt.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(541, 30);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 24);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Böngészés";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // eredmenytxt
+            // 
+            this.eredmenytxt.Location = new System.Drawing.Point(32, 63);
+            this.eredmenytxt.Margin = new System.Windows.Forms.Padding(2);
+            this.eredmenytxt.Multiline = true;
+            this.eredmenytxt.Name = "eredmenytxt";
+            this.eredmenytxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.eredmenytxt.Size = new System.Drawing.Size(505, 318);
+            this.eredmenytxt.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(541, 63);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(175, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Népesség fájl";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(32, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Záróév";
+            // 
+            // zaroev
+            // 
+            this.zaroev.Location = new System.Drawing.Point(81, 33);
+            this.zaroev.Margin = new System.Windows.Forms.Padding(2);
+            this.zaroev.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.zaroev.Name = "zaroev";
+            this.zaroev.Size = new System.Drawing.Size(90, 20);
+            this.zaroev.TabIndex = 7;
+            this.zaroev.Value = new decimal(new int[] {
+            2025,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(645, 410);
+            this.Controls.Add(this.csvpathtxt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.eredmenytxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zaroev);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.zaroev)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox csvpathtxt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox eredmenytxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown zaroev;
     }
 }
 
--- week07/Form1.cs
+++ week07/Form1.cs
@@ -24,7 +24,66 @@ namespace week07
             Population = GetPopulation(@"C:\Windows\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Windows\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Windows\Temp\halál.csv");
+
+
+            
         }
+
+        private void Simulation(int zaroev)
+        {
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    Random rng = new Random(1234);
+                    Person person = Population[i];
+                    //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+                    if (!person.IsAlive) return;
+
+                    // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+                    byte age = (byte)(year - person.BirthYear);
+
+                    // Halál kezelése
+                    // Halálozási valószínűség kikeresése
+                    double pDeath = (from x in DeathProbabilities
+                                     where x.Gender == person.Gender && x.Age == age
+                                     select x.DeathProb).FirstOrDefault();
+                    // Meghal a személy?
+                    if (rng.NextDouble() <= pDeath)
+                        person.IsAlive = false;
+
+                    //Születés kezelése - csak az élő nők szülnek
+                    if (person.IsAlive && person.Gender == Gender.Female)
+                    {
+                        //Szülési valószínűség kikeresése
+                        double pBirth = (from x in BirthProbabilities
+                                         where x.Age == age
+                                         select x.BirthProb).FirstOrDefault();
+                        //Születik gyermek?
+                        if (rng.NextDouble() <= pBirth)
+                        {
+                            Person újszülött = new Person();
+                            újszülött.BirthYear = year;
+                            újszülött.NbrOfChildren = 0;
+                            újszülött.Gender = (Gender)(rng.Next(1, 3));
+                            Population.Add(újszülött);
+                        }
+                    }
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                //Console.WriteLine(
+                // string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+                eredmenytxt.Text = eredmenytxt.Text + string.Format("Szimulációs év:{0}" + Environment.NewLine + "\t" + " Fiuk:{1}" + Environment.NewLine + "\t" + " Lányok:{2}" + Environment.NewLine, year, nbrOfMales, nbrOfFemales);
+            }
+        }
+
         public List<Person> GetPopulation(string csvpath)
         {
             List<Person> population = new List<Person>();
@@ -83,6 +142,32 @@ namespace week07
             }
             return DeathProbs;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            eredmenytxt.Text = "";
+            Simulation(int.Parse(zaroev.Value.ToString()) + 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = false;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                string sFileName = choofdlog.FileName;
+                csvpathtxt.Text = choofdlog.FileName.ToString();
+
+            }
+            Population = GetPopulation(choofdlog.FileName);
+
+
+
+        }
+
         private void SimStep(int year, Person person)
         {
             Random rng = new Random(1234);
